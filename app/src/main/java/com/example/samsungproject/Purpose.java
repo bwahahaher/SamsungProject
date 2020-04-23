350ab22a80ec8777a18dfcb72c62c1df66bd7af6
Vladislav Shemarin <batsyhc@gmail.com>
2020-04-23 15:09:36 +0500

    dadasdas

---
+++ app/src/main/java/com/example/samsungproject/Purpose.java
@@ -0,0 +1,13 @@
+package com.example.samsungproject;
+
+public class Purpose {
+ public String deadline, head, stat;
+ public int id;
+
+    public Purpose(int id, String deadline, String head, String stat) {
+        this.id = id;
+        this.deadline = deadline;
+        this.head = head;
+        this.stat = stat;
+    }
+}
--- app/src/main/java/com/example/samsungproject/fragments/RecycleAdapter.java
+++ app/src/main/java/com/example/samsungproject/fragments/RecycleAdapter.java
@@ -1,5 +1,4 @@
 package com.example.samsungproject.fragments;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,53 +7,52 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.samsungproject.Challenge;
-import com.example.samsungproject.Challenge;
+import com.example.samsungproject.Purpose;
 import com.example.samsungproject.R;
 
 import java.util.ArrayList;
 
 class RecycleAdapter extends RecyclerView.Adapter<ContactVH> {
-    final ArrayList<Challenge> challengeList;
+    final ArrayList<Purpose> purposeList;
 
-    public RecycleAdapter(ArrayList<Challenge> challenges) {
-        this.challengeList = challenges;
+    public RecycleAdapter(ArrayList<Purpose> userList) {
+        this.purposeList = userList;
     }
 
 
     @NonNull
     @Override
     public ContactVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View contactView = LayoutInflater.from(parent.getContext()).inflate(R.layout.challenge_view, parent, false);
+        View contactView = LayoutInflater.from(parent.getContext()).inflate(R.layout.purpose_view, parent, false);
         return new ContactVH(contactView);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ContactVH holder, int position) {
-        Challenge challenge = challengeList.get(position);
-        holder.bind(challenge);
+        Purpose purpose = purposeList.get(position);
+        holder.bind(purpose);
     }
 
     @Override
     public int getItemCount() {
-        return challengeList.size();
+        return purposeList.size();
     }
 }
 
 class ContactVH extends RecyclerView.ViewHolder {
-    TextView name, duration, hard;
+    TextView deadline, head, stat;
 
     public ContactVH(@NonNull View itemView) {
         super(itemView);
-        name = itemView.findViewById(R.id.name);
-        duration = itemView.findViewById(R.id.duration);
-        hard = itemView.findViewById(R.id.hard);
+        deadline = itemView.findViewById(R.id.deadline);
+        stat = itemView.findViewById(R.id.stat);
+        head = itemView.findViewById(R.id.head);
 
     }
 
-    public void bind(Challenge challenge) {
-        name.setText("test");
-        duration.setText("ksnd");
-        hard.setText("dasdasdas");
+    public void bind(Purpose purpose) {
+        deadline.setText(purpose.deadline);
+        stat.setText(purpose.stat);
+        head.setText(purpose.head);
     }
 }
